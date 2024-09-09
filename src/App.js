739f2415630304dcf8b50ddb342d242159e7bc9e
Suprith Singh <123349613+suprith03@users.noreply.github.com>
2024-09-09 23:09:01 +0530

    practicing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+const IncreDecre = () => {
+    const[num, setNum] = useState(0)
+    const increment = () => {
+        setNum(num + 1)
+    }
+    const decrement = () => {
+        if(num === 0) {
+            alert('Number cannot be less than 0')
+        }
+        else{
+        setNum(num - 1)
+        }
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='conatiner'>
+        <h1>{num}</h1>
+      <div className='btn'></div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default IncreDecre
